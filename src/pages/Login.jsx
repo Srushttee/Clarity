a5fc9fe5e390aff9216cd468a8cb08279fbd3892
Srushttee <srushtitatte66@gmail.com>
2026-01-04 14:38:58 +0530

    Login Page updated and Registration Page added.Added animations and routes.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,13 @@
 import "../styles/login.css";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigate = useNavigate();
+
   return (
     <div className="login-container">
-      {/* Left image section */}
-      <div className="login-image"><img src="assets/Login.jpg" alt="" /></div>
-
-      {/* Right form section */}
-      <div className="login-form-section">
-        <h1 className="login-heading">Login</h1>
+      <div className="login-card">
+        <h1 className="login-heading fade-title">Login</h1>
 
         <div className="input-group">
           <span className="icon">✉</span>
@@ -18,13 +17,16 @@ function Login() {
         <div className="input-group">
           <span className="icon">🔒</span>
           <input type="password" placeholder="Password" />
-          <span className="forgot">Forgot Password ?</span>
+          <span className="forgot">Forgot Password?</span>
         </div>
 
         <button className="login-btn">Login</button>
 
         <p className="signup-text">
-          Don't have an account ? <span>Sign Up here .</span>
+          Don't have an account?{" "}
+          <span onClick={() => navigate("/signup")}>
+            Sign Up here.
+          </span>
         </p>
       </div>
     </div>
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,38 @@
+import "../styles/signup.css";
+
+function Signup() {
+  return (
+    <div className="signup-container">
+      <div className="signup-card">
+        <h1 className="signup-heading">CREATE AN ACCOUNT</h1>
+
+        <div className="input-group">
+          <span className="icon">✉</span>
+          <input type="text" placeholder="Username" />
+        </div>
+
+        <div className="input-group">
+          <span className="icon">🔒</span>
+          <input type="password" placeholder="Password" />
+        </div>
+
+        <div className="input-group">
+          <span className="icon">🔒</span>
+          <input type="password" placeholder="Confirm Password" />
+        </div>
+
+        <div className="input-group">
+          <span className="icon">⚙</span>
+          <input type="text" placeholder="Admin Token ( Optional )" />
+        </div>
+
+        <div className="button-group">
+          <button className="primary-btn">Register</button>
+          <button className="secondary-btn">Register as Admin</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
